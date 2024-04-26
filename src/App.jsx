39b7c402ccb3c00bc33380dd7d9072819cc84fb6
Volodymyr <volodymyr.blyntsov@gmail.com>
2024-04-26 16:02:22 +0300

    Web Notes
    Added the functionality of creating a note.

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Notes from './pages/Notes'
 import CreateNote from './pages/CreateNote'
 import EditNote from './pages/EditNote'
+import dummyNotes from './dummy_notes'
+
+
 
 const App = () => {
-  return (
-    <main id="app">
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={ <Notes /> } />
-                <Route path="/create-note" element={ <CreateNote /> } />
-                <Route path="/edit-note/:id" element={ <EditNote /> } />
-            </Routes>
-    </BrowserRouter>
-    </main>
-  )
+    const [notes, setNotes] = useState(dummyNotes)
+    
+    return (
+        <main id="app">
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={ <Notes notes={notes} /> } />
+                    <Route path="/create-note" element={ <CreateNote /> } />
+                    <Route path="/edit-note/:id" element={ <EditNote /> } />
+                </Routes>
+        </BrowserRouter>
+        </main>
+    )
 }
 
 export default App
--- src/pages/CreateNote.jsx
+++ src/pages/CreateNote.jsx
@@ -1,20 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { IoIosArrowBack } from 'react-icons/io'
 import { Link } from 'react-router-dom'
+import { v4 as uuid } from 'uuid'
 
 const CreateNote = () => {
-  return (
-    <section>
-        <header className="create-note__header">
-            <Link to="/" className='btn'> <IoIosArrowBack /> </Link>
-            <button className="btn lg primary">Зберегти</button>
-        </header>
-        <form className="create-note__form">
-            <input type="text" placeholder='Назва' autoFocus />
-            <textarea rows="28" placeholder='Деталі нотатки...'></textarea>
-        </form>
-    </section>
-  )
+
+    const [title, setTitle] = useState('')
+    const [details, setDetails] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        if (title && details) {
+            const note = {id: uuid(), title, details }
+            console.log(note)
+        }
+    }
+
+    return (
+        <section>
+            <header className="create-note__header">
+                <Link to="/" className='btn'> <IoIosArrowBack /> </Link>
+                <button className="btn lg primary" onClick={handleSubmit}>Зберегти</button>
+            </header>
+            <form className="create-note__form" onSubmit={handleSubmit}>
+                <input type="text" placeholder='Назва' value={title} onChange={e => setTitle(e.target.value)} autoFocus />
+                <textarea rows="28" placeholder='Деталі нотатки...' value={details} onChange={e => setDetails(e.target.value)}></textarea>
+            </form>
+        </section>
+    )
 }
 
 export default CreateNote
