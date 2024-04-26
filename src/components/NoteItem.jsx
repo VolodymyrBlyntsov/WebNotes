3415c12a44a17408e61feb2f14a6dc599827acde
Volodymyr <volodymyr.blyntsov@gmail.com>
2024-04-26 17:43:15 +0300

    Update NoteItem.jsx
    AYO! Unique colorful colors for each note have been dropped!

--- src/components/NoteItem.jsx
+++ src/components/NoteItem.jsx
@@ -2,8 +2,17 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const NoteItem = ({note}) => {
+  // Функція для генерації випадкового кольору
+  const getRandomColor = () => {
+    const colors = ['#7634d8', '#f98412', '#3498db', '#2ecc71', '#e74c3c', '#9b59b6', '#f1c40f'];
+    return colors[Math.floor(Math.random() * colors.length)];
+  };
+
+  // Отримуємо випадковий колір
+  const randomColor = getRandomColor();
+
   return (
-    <Link to={`/edit-note/${note.id}`} className='note'> 
+    <Link to={`/edit-note/${note.id}`} className='note' style={{ background: randomColor }}> {/* Встановлюємо стиль фону */}
         <h4>{note.title.length > 60 ? (note.title.substr(0, 60)) + '...' : note.title}</h4>
         <p>{note.date}</p>
     </Link>
