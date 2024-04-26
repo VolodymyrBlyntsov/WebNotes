f7e37902ac7b0b403e651aff89ec0e5a375e304d
Volodymyr <volodymyr.blyntsov@gmail.com>
2024-04-26 15:35:30 +0300

    Web Notes
    Create a design for the CreateNote component.

--- src/pages/CreateNote.jsx
+++ src/pages/CreateNote.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { IoIosArrowBack } from 'react-icons/io'
+import { Link } from 'react-router-dom'
 
 const CreateNote = () => {
   return (
-    <div>CreateNote</div>
+    <section>
+        <header className="create-note__header">
+            <Link to="/" className='btn'> <IoIosArrowBack /> </Link>
+            <button className="btn lg primary">Зберегти</button>
+        </header>
+        <form className="create-note__form">
+            <input type="text" placeholder='Назва' autoFocus />
+            <textarea rows="28" placeholder='Деталі нотатки...'></textarea>
+        </form>
+    </section>
   )
 }
 
