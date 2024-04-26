d7bc4d48812375595338a29905d3581fceaf42fc
Volodymyr <volodymyr.blyntsov@gmail.com>
2024-04-26 15:45:18 +0300

    Web Notes
    Create a design for the EditNote component.

--- src/pages/EditNote.jsx
+++ src/pages/EditNote.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { RiDeleteBin6Line } from 'react-icons/ri'
+import { IoIosArrowBack } from 'react-icons/io'
+import { Link } from 'react-router-dom'
 
 const EditNote = () => {
-  return (
-    <div>EditNote</div>
-  )
+    return (
+        <section>
+            <header className="create-note__header">
+                <Link to="/" className='btn'> <IoIosArrowBack /> </Link>
+                <button className="btn lg primary">Зберегти</button>
+                <button className='btn danger'> <RiDeleteBin6Line /> </button>
+            </header>
+            <form className="create-note__form">
+                <input type="text" placeholder='Назва' autoFocus />
+                <textarea rows="28" placeholder='Деталі нотатки...'></textarea>
+            </form>
+        </section>
+      )
 }
 
 export default EditNote
