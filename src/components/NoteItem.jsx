0115784259fbe79403783382bb49eb8f30ad88aa
Volodymyr <volodymyr.blyntsov@gmail.com>
2024-04-26 15:20:16 +0300

    Web Notes
    Work has begun on creating components for notes and drawing them.
    A blank for drawing each note has been created.

---
+++ src/components/NoteItem.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NoteItem = ({note}) => {
+  return (
+    <div>NoteItem</div>
+  )
+}
+
+export default NoteItem
--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { CiSearch } from 'react-icons/ci'
+import NoteItem from '../components/NoteItem'
+import dummyNotes from '../dummy_notes'
+import { Link } from 'react-router-dom' 
+import { BsPlusLg } from 'react-icons/bs'
 
 const Notes = () => {
   return (
-    <div>Notes</div>
+    <section>
+        <header className='notes__header'>
+            <h2>Нотатки</h2>
+            {/* <input type="text" autoFocus placeholder='Введіть...'/> */}
+            <button className='btn'> <CiSearch /> </button>
+        </header>
+        <div className="notes__container">
+            {
+                dummyNotes.map(note => <NoteItem key={note.id} note={note} />)
+            }
+        </div>
+        <Link className='btn add__btn'> <BsPlusLg /> </Link>
+    </section>
   )
 }
 
